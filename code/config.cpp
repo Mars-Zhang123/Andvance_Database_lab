4fa1a082d56e1cce9a93a7257ebba5acf29ee280
Mars Zhang <1802257366@qq.com>
2021-11-03 15:13:06 +0800

    INIT

---
+++ code/config.cpp
@@ -0,0 +1,13 @@
+#include "config.h"
+#include <iostream>
+using namespace std;
+
+const extern int FRAMESIZE = 4096;
+const extern int DEFBUFSIZE = 1024;
+
+int main(){
+
+    cout << "hello \n";
+    cin.get();
+    return 0;
+}
---
+++ code/config.h
@@ -0,0 +1,8 @@
+#pragma once
+//帧的大小
+const extern int FRAMESIZE;
+
+//缓冲区大小（帧的个数）
+const extern int DEFBUFSIZE;
+
+
